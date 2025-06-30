eeb180a7c3bde71879e99f53f465a0dc75b3bd82
ape7054 <1469041017@qq.com>
2025-06-30 15:28:48 +0800

    更新前端部署路径为market-pulse，并为前端部署工作流添加路径限制

---
+++ backend/temp-setup.sql
@@ -0,0 +1,5 @@
+
+CREATE DATABASE IF NOT EXISTS market_pulse_db;
+CREATE USER IF NOT EXISTS 'market_pulse_user'@'localhost' IDENTIFIED BY 'wBYXZkiLTExiEAHF';
+GRANT ALL PRIVILEGES ON market_pulse_db.* TO 'market_pulse_user'@'localhost';
+FLUSH PRIVILEGES;
