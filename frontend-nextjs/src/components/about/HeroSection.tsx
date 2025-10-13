002c53b91a82fbce0a123db5c6910b3c1a3cdcc0
ape7054 <1469041017@qq.com>
2025-10-14 00:22:05 +0800

    fix: 调整MatrixBackground组件的密度属性\n\n- 将密度值从0.7更新为1.5，以改善背景效果

--- frontend-nextjs/src/components/about/HeroSection.tsx
+++ frontend-nextjs/src/components/about/HeroSection.tsx
@@ -31,7 +31,7 @@ export function HeroSection() {
     <MatrixBackground 
       className="h-auto min-h-[500px] md:min-h-[600px] border-b border-border/40"
       speed={0.8}
-      density={0.7}
+      density={1.5}
       brightness={0.6}
       greenIntensity={0.8}
       variation={0.8}
